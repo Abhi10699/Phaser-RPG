88d69577d1b6fb31b73e7e28d03b464ef36c3e4c
Abhi10699 <patelabhi10699@gmail.com>
2019-05-31 02:10:22 +0530

    Added movement animations :)

--- src/Player/Player.main.js
+++ src/Player/Player.main.js
@@ -5,7 +5,7 @@ import AnimManager from '../index';
 export default class Player extends GameObjects.Sprite{
   constructor(scene,x,y,key){
     super(scene,x,y,key);
-    this.setFrame(25);
+    this.setFrame(3);
     this.setScale(1.5,1.5);
     
     this.movementHandler = scene.input.keyboard.createCursorKeys();
@@ -19,23 +19,27 @@ export default class Player extends GameObjects.Sprite{
     const dir = new Phaser.Math.Vector2();
     if(this.movementHandler.up.isDown){
       dir.y = -1;
-      this.anims.play('move-up',true);
+      this.anims.play('walk-up',true);
     }
 
     else if(this.movementHandler.down.isDown){
       dir.y = 1;
-      // this.anims.play('walk-down',true);
+      this.anims.play('walk-down',true);
     }
     else if(this.movementHandler.left.isDown){
       dir.x = -1;
+      this.anims.play('walk-left',true);
+      
     }
     else if(this.movementHandler.right.isDown){
       dir.x = 1;
+      this.anims.play('walk-right',true);
+
     }
 
     else{
       dir.set(0,0);
-      // this.anims.stop();
+      this.anims.stop();
     }
 
     this.body.setVelocity(dir.x * this.movementSpeed,dir.y * this.movementSpeed);
