848595c118024d0ada7e77e7cab1717ef67fd929
Abhi10699 <patelabhi10699@gmail.com>
2019-06-02 22:17:27 +0530

    Removed unwanted file

--- src/Player/Player.animations.js
+++
@@ -1,9 +0,0 @@
-import Phaser,{Animations} from 'phaser';
-import AnimationHandler from '../index'
-
-
-const {Animation,AnimationFrame} = Animations;
-
-const moveUp = new Animation(AnimationHandler,'walk-up',{
-
-})
