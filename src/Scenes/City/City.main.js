fd32fbe5068198835c94a57328e08421e6342462
Abhi10699 <patelabhi10699@gmail.com>
2019-06-02 13:00:59 +0530

    removed a street layer...

--- src/Scenes/City/City.main.js
+++ src/Scenes/City/City.main.js
@@ -12,19 +12,15 @@ export default class City extends Scene{
     this.map = this.make.tilemap({key:'world'});
     this.tileset = this.map.addTilesetImage("terrain_atlas","tiles",32,32,0,0);
     this.bgLayer = this.map.createStaticLayer("Background",this.tileset,0,0);
-    this.flowersLayer = this.map.createStaticLayer("Tile Layer 2",this.tileset,0,0);
-
-    // this.bgLayer.setDepth(-1);
-    
-    // Load Player 
+      // Load Player 
 
     this.player = new Player(this,100,100,'player');
     this.physics.world.enableBody(this.player);
-    // this.cameras.main.startFollow(this.player)
+    this.cameras.main.startFollow(this.player)
 
 
-    // this.cameras.main.setBounds(0,0,this.cameras.main.width + this.tileset.tileWidth + this.player.x,this.cameras.main.height + this.tileset.tileHeight + this.player.y)
-    
+    this.cameras.main.setBounds(0,0,this.bgLayer.width * this.bgLayer.scaleX,this.bgLayer.height * this.bgLayer.scaleY);
+  
   }
 
   update(){
