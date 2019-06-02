0919e06f0390e9fc07aaaf1a8f48113ad90fdb6b
Abhi10699 <patelabhi10699@gmail.com>
2019-06-02 17:33:12 +0530

    Changed tileset image key to new map keys

--- src/Scenes/City/City.main.js
+++ src/Scenes/City/City.main.js
@@ -7,20 +7,19 @@ export default class City extends Scene{
   }
   create(){
     this.movements = this.input.keyboard.createCursorKeys();
- 
+    
     // Tile maps
     this.map = this.make.tilemap({key:'world'});
-    this.tileset = this.map.addTilesetImage("terrain_atlas","tiles",32,32,0,0);
+    this.tileset = this.map.addTilesetImage("worldFix","tiles",32,32,1,2);
     this.bgLayer = this.map.createStaticLayer("Background",this.tileset,0,0);
+   // this.farmLayer = this.map.createStaticLayer("FarmBlocks",this.tileset,0,0);
       // Load Player 
 
-    this.player = new Player(this,100,100,'player');
+    this.player = new Player(this,200,200,'player');
     this.physics.world.enableBody(this.player);
     this.cameras.main.startFollow(this.player)
 
-
     this.cameras.main.setBounds(0,0,this.bgLayer.width * this.bgLayer.scaleX,this.bgLayer.height * this.bgLayer.scaleY);
-  
   }
 
   update(){
